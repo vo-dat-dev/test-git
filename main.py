1d2ee38e1d2d464441333d744c41c839d665b98a
Võ Tiến <votiendat08112001@gmail.com>
2025-05-21 11:13:11 +0700

    Revert "feat: commit 3"
    This reverts commit e1522ec265831ff4759fe4ff77c71641a7e0ef3c.

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('this branch is main')
+    print('commit 1')
+    print('commit 2')
+    print('commit 3')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
